7bbdee2defae9d69769eddc02a60134134bb4ce7
Jess Crees <jessicacrees@gmail.com>
2023-10-10 08:14:43 +0100

    Add text that will be needed to language file

---
+++ src/constants/language.ts
@@ -0,0 +1,13 @@
+export const HEADING_CAE = 'CAE'
+export const HEADING_RESULTS = 'Results'
+
+export const ANSWER_LABEL_CORRECT = 'Correct'
+export const ANSWER_LABEL_INCORRECT = 'False'
+
+export const BUTTON_LABEL_CORRECT = 'Correct'
+export const BUTTON_LABEL_INCORRECT = 'Incorrect'
+
+export const QUESTION_LABEL = 'Q'
+
+export const NAVIGATION_LABEL_HOME = 'Home'
+export const NAVIGATION_LABEL_RESULTS = 'Results'
